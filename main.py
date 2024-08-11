042d9a2682d4e1b06b8ebf1e9200071e2377a46e
moKhaled73 <mokhaled732003@gmail.com>
2024-08-11 22:46:11 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+import tensorflow as tf
+import keras
+from PIL import Image
+import io
+
+# Load the trained model
+model = tf.keras.models.load_model('minst_model.h5')
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+)
+
+@app.post("/predict/")
+async def predict_digit(file: UploadFile = File(...)):
+    try:
+        # Read the uploaded image file
+        image_data = await file.read()
+        # print(image_data)
+        image = Image.open(io.BytesIO(image_data)).convert('L')  # Convert to grayscale
+        
+        # Resize and preprocess the image
+        image = image.resize((28, 28))
+        image_array = np.array(image, dtype=np.float32).reshape(1, 28, 28, 1) / 255.0
+
+        # Make prediction
+        prediction = model.predict(image_array)
+        predicted_label = np.argmax(prediction)
+
+        return {"predicted_label": int(predicted_label)}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+
+
+@app.get("/")
+def root():
+    return {'hello': 'world'}
